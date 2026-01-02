4c3b61a84342887c6a3db1c4f4a45ca439ee4480
uyenhoang99 <uhoang67@gmail.com>
2026-01-02 02:01:55 -0800

    update file

---
+++ src/components/data_transformation.py
@@ -0,0 +1,21 @@
+import sys
+import os
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+
+@dataclass
+class DataTransformationConfig:
+    processor_ob_file_path = os.path.join('artifacts', "preprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+        

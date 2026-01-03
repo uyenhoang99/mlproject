049d7512866ca5891c20ef94ca4c02f31ee3a209
uyenhoang99 <uhoang67@gmail.com>
2026-01-03 14:37:53 -0800

    update files

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -95,7 +95,7 @@ class DataTransformation:
             return(
                 train_arr,
                 test_arr,
-                self.data_transformation_config.preprocessor_obj_file_path
+                self.data_transformation_config.preprocessor_obj_file_path,
             )
 
 

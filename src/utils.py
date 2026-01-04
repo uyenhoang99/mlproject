b363cf2018ba21ea21ec30615a38d337bef78feb
uyenhoang99 <uhoang67@gmail.com>
2026-01-03 16:04:41 -0800

    update files

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -19,14 +20,21 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, param, cv = 3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
             
-            model.fit(X_train, y_train) #Train model
+            # model.fit(X_train, y_train) #Train model
 
             y_train_pred = model.predict(X_train)
 
